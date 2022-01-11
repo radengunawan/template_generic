b5173825441621a55312db9fde292e937ad332a0
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-11 16:04:08 +0800

    with UserINput

--- Template_Console/Program.cs
+++ Template_Console/Program.cs
@@ -24,8 +24,13 @@ namespace template_generic
         static void Main(string[] args)
         {    //---------START AFTER THIS LINE-------------START AFTER THIS LINE------------START AFTER THIS LINE--------------------
 
-            
 
+            char sender = Console.ReadKey(true).KeyChar;
+            int sender_index = (int)sender - 48;
+
+            Console.Write("\nReceiver index: ");
+            char receiver = Console.ReadKey(true).KeyChar;
+            int receiver_index = (int)receiver - 48;
 
             // ---- DON'T WRITE BEYOND THIS LINE------------
         }// END of Main program
