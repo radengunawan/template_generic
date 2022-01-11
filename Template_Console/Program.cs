f9d8f50de962b577f05f31260f55deee1702dba9
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-11 16:02:26 +0800

    Add project files.

---
+++ Template_Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace template_generic
+{
+
+    class Gajah
+    {
+        public int xxxx;
+        public string yyyy;
+
+        public void WhoAmI()
+        {
+            Console.WriteLine("My name is " + Name + ".");
+            Console.WriteLine("My ears are " + EarSize + " inches tall.");
+        }
+
+
+    } // end of class
+
+
+
+    // BEGIN of Main Class
+    class Program
+    {
+        static void Main(string[] args)
+        {    //---------START AFTER THIS LINE-------------START AFTER THIS LINE------------START AFTER THIS LINE--------------------
+
+            
+
+
+            // ---- DON'T WRITE BEYOND THIS LINE------------
+        }// END of Main program
+
+    } //END of Main class
+} //END of namespace
